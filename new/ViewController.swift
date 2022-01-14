b9d39b9bdb182cadf0ce313381ce49712171210a
Alvie <alvieforwork@gmail.com>
2022-01-14 14:07:04 +0800

    Initial Commit

---
+++ new/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  new
+//
+//  Created by worker on 2022/1/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
